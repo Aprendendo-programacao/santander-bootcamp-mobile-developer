88a3075a10565fd68b2f2ad65db0fce56c7a7c32
ImGabreuw <imgabreuw.dev@gmail.com>
2021-06-27 17:38:41 -0300

    Aula: 'Proposta de exercicio final'

---
+++ introducao-ao-kotlin/proposta-de-exercicio-final/src/main/kotlin/main.kt
@@ -0,0 +1,13 @@
+/*
+    Exercício final
+
+    Com os conhecimentos aplicados anteriormente, vamos criar uma calculadora. A partir
+    de 2 valores "Flot" e um outro valor que correspodende a operação, retorne e imprima
+    o resultado com Float ou uma mensagem de erro caso um dos valores seja nulo.
+
+    Use sua criatividade! (adicione operações como: fatorial, porcentagem, potenciação)
+ */
+
+fun main(args: Array<String>) {
+
+}
