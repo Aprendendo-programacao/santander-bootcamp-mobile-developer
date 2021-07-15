b7c5812b72d80518fdd9c7c4f153e888557f45bd
ImGabreuw <imgabreuw.dev@gmail.com>
2021-07-15 17:58:06 -0300

    Aula: 'Desenvolvendo a MainActivity'

--- trabalhando-com-o-android-studio-e-principais-ferramentas/projects/calculadoradenotas/app/src/main/java/com/example/calculadora_de_notas/MainActivity.kt
+++ trabalhando-com-o-android-studio-e-principais-ferramentas/projects/calculadoradenotas/app/src/main/java/com/example/calculadora_de_notas/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.calculadora_de_notas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val calcularButton = calcular
+        val resultado = resultado
+
+        calcularButton.setOnClickListener {
+            val nota1 = Integer.parseInt(nota1.text.toString())
+            val nota2 = Integer.parseInt(nota2.text.toString())
+            val media = (nota1 + nota2) / 2
+
+            val faltas = Integer.parseInt(faltas.text.toString())
+
+            if (media >= 6 && faltas <= 10) {
+                resultado.setText("""
+                    Aluno foi aprovado 
+                    Nota final: ${media}
+                    Faltas: ${faltas}
+                """.trimIndent())
+            } else {
+                resultado.setText("""
+                    Aluno foi reprovado 
+                    Nota final: ${media}
+                    Faltas: ${faltas}
+                """.trimIndent())
+            }
+        }
+
     }
+
 }
