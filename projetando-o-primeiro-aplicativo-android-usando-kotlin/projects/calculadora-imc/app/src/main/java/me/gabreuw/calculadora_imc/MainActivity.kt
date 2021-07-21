e45be59a92b8f3603e608852431184831105c873
ImGabreuw <imgabreuw.dev@gmail.com>
2021-07-21 19:17:46 -0300

    Desafio finalizado: calculadora IMC

--- projetando-o-primeiro-aplicativo-android-usando-kotlin/projects/calculadora-imc/app/src/main/java/me/gabreuw/calculadora_imc/MainActivity.kt
+++ projetando-o-primeiro-aplicativo-android-usando-kotlin/projects/calculadora-imc/app/src/main/java/me/gabreuw/calculadora_imc/MainActivity.kt
@@ -1,6 +1,7 @@
 package me.gabreuw.calculadora_imc
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doAfterTextChanged
@@ -15,18 +16,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setListeners() {
-        heightET?.doAfterTextChanged { text ->
-//            Toast.makeText(this, text.toString(), Toast.LENGTH_SHORT).show()
-        }
-
-        weightET.doOnTextChanged { text, _, _, _ ->
-//            titleTXT.text = text
-        }
-
         calculate_imc_btn.setOnClickListener {
             calculateIMC(weightET.text.toString(), heightET.text.toString())
         }
-
     }
 
     private fun calculateIMC(weight: String, height: String) {
@@ -35,7 +27,9 @@ class MainActivity : AppCompatActivity() {
 
         if (weight != null && height != null) {
             val imc = weight / (height * height)
+
             titleTXT.text = "Seu IMC é de %.2f".format(imc)
+            imc_rating.visibility = View.VISIBLE
         }
     }
 }
