e6dcb14b252c1cac152ef5b73ce55f47fd67d4e8
ImGabreuw <imgabreuw.dev@gmail.com>
2021-07-26 17:39:37 -0300

    Aula: 'Navigation Drawer'

--- conceito-de-navegacao-aparencia-e-estilo-de-aplicativos-android/projects/lista-de-contatos/app/src/main/java/me/gabreuw/lista_de_contatos/MainActivity.kt
+++ conceito-de-navegacao-aparencia-e-estilo-de-aplicativos-android/projects/lista-de-contatos/app/src/main/java/me/gabreuw/lista_de_contatos/MainActivity.kt
@@ -5,7 +5,11 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -19,10 +23,28 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.drawer_menu)
 
+        initDrawer()
         bindViews()
         updateContactList()
+    }
+
+    private fun initDrawer() {
+        val drawerLayout = findViewById<View>(R.id.drawer_layout) as DrawerLayout
+        val toolBar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolBar)
+
+        val toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolBar,
+            R.string.open_drawer,
+            R.string.close_drawer
+        )
+
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
     }
 
     private fun bindViews() {
