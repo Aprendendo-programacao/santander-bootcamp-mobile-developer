06f556750ba7fcb976fd762e5f52f6f30d76f8b3
ImGabreuw <imgabreuw.dev@gmail.com>
2021-07-27 16:44:40 -0300

    Aula: 'Regras de negócios'

--- viewmodel-e-lifecycle-para-melhorar-experiencia-em-aplicativos-android/projects/viewmodel/app/src/main/java/me/gabreuw/viewmodel/MainActivity.kt
+++ viewmodel-e-lifecycle-para-melhorar-experiencia-em-aplicativos-android/projects/viewmodel/app/src/main/java/me/gabreuw/viewmodel/MainActivity.kt
@@ -2,10 +2,57 @@ package me.gabreuw.viewmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var txtContador: EditText
+    lateinit var btnDados: Button
+    lateinit var btnMostrar: Button
+
+    var contador = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initDados()
+        initContador()
+        initClick()
+
+        validaContador()
     }
+
+    private fun validaContador() {
+        if (contador > 5) {
+            contador = 0
+        }
+    }
+
+    private fun initClick() {
+        btnDados.setOnClickListener {
+            contador++
+            validaContador()
+            initContador()
+        }
+
+        btnMostrar.setOnClickListener {
+            Toast.makeText(
+                this,
+                "Valor contador: $contador",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
+
+    private fun initContador() = txtContador.setText(contador.toString())
+
+    private fun initDados() {
+        txtContador = findViewById(R.id.txtContador)
+        btnDados = findViewById(R.id.btnDados)
+        btnMostrar = findViewById(R.id.btnMostrar)
+    }
+
 }
